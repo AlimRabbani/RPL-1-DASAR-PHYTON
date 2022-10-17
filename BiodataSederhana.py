5a3dc8ecadff84da14aa5af24b59bd3575d37ecb
AlimRabbani <alimmanurung18@gmail.com>
2022-10-18 05:20:11 +0800

    day 2

---
+++ BiodataSederhana.py
@@ -0,0 +1,8 @@
+nama ="Alim rabbani" 
+namaDepan ="alim" 
+umur = 19 
+hoby = "Berenang" 
+print("nama :" , nama) 
+print("nama panggilan  :", namaDepan) 
+print("umur :" , umur) 
+print("hobby :", hoby) 
---
+++ Hellow Word.py
@@ -0,0 +1,3 @@
+print ("hellow word") 
+print ("hellow nama saya alim rabbani") 
+print ("umur saya 19 tahun") 
