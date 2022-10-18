6dd0926df8bda571c5315bf469b4db24cb6f64b8
AlimRabbani <alimmanurung18@gmail.com>
2022-10-19 05:38:18 +0800

    rpl3

---
+++ operatorDasar.py
@@ -0,0 +1,27 @@
+#operator aritmatika 
+#penjumlahan 
+#contoh
+apel = 2
+anggur = 3 
+jeruk = 10 
+melon = 5
+semangka = 10 
+hasil = apel + anggur  + jeruk + melon+semangka 
+print("hasilnya adalah : " , hasil) 
+
+#pengurangan 
+#contoh 1
+angka1 = 100 
+angka2 = 10 
+angka3 = 20 
+hasil = angka1 - angka2 - angka3  
+print(hasil) 
+
+#contoh 2
+hutang = 100000 
+bayar1 = 10000
+utang = 50000 
+bayar2 = 10000 
+sisaHutangSaya = hutang - bayar1 + utang -bayar2 
+print(" sisa hutang saya sebesar :" , sisaHutangSaya) 
+
