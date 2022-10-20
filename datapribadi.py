cfa64e6eb00b1515f75a111e27e2ff0c0cc1f84e
AlimRabbani <alimmanurung18@gmail.com>
2022-10-20 08:46:23 +0800

    RPL day 4

---
+++ datapribadi.py
@@ -0,0 +1,10 @@
+nama ="ADRIAWAN" 
+namaDepan ="ADRI" 
+umur = 21
+status = "mahasiswa"
+hoby = "Berenang" 
+print("nama :" , nama) 
+print("nama panggilan  :", namaDepan) 
+print("umur :" , umur)
+print("status :",status) 
+print("hobby :", hoby) 
---
+++ operatorDasar2.py
@@ -0,0 +1,22 @@
+#operator aritmatika Dasar 2 
+#perkalian 
+angka1 = 10 
+angka2 = 2 
+hasilPerkalian = angka1 * angka2 
+print("hasil perkalian angka :", hasilPerkalian) 
+#contoh 1 
+panjang = 10 
+lebar = 2 
+luas = panjang * lebar 
+print("luas : ",luas)
+
+#pembagian 
+bilangan1 = 20 
+bilangan2 = 5
+hasil = bilangan1 / bilangan2 
+print("hasil :" , hasil) 
+#contoh 1
+apel = 10
+anak = 7
+apelPeranak = apel / anak 
+print("maka masing masiang anak akan mendapatkan apel sebanyak :" ,apelPeranak)
